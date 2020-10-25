aa5a01012221b74714e387158c327246663fc884
Patricio López Juri <lopezjuripatricio@gmail.com>
2020-10-25 13:48:24 -0300

    Fix lint issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,19 +15,15 @@ module.exports = {
     sourceType: "module", // Allows for the use of imports
   },
   settings: {},
-  env: {
-    "jest/globals": true,
-  },
+  env: {},
   extends: [
     "oclif",
     "oclif-typescript",
     "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     "plugin:prettier/recommended", // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-    "plugin:jest/recommended",
-    "plugin:jest/style",
   ],
-  plugins: ["jest"],
+  plugins: [],
   rules: {
     eqeqeq: "error",
     "no-alert": "warn",
