de8e7c2fdfb05dfeee41627a22b30209ed3a7210
Patricio López Juri <lopezjuripatricio@gmail.com>
2020-12-31 09:35:46 -0300

    fix: Fix Tailwind purge list

--- templates/react-typescript-tailwind/tailwind.config.js
+++ templates/react-typescript-tailwind/tailwind.config.js
@@ -1,8 +1,21 @@
 module.exports = {
-  purge: [
-    "./(templates/components)/**/*.(js|jsx|ts|tsx)",
-    "./styles/**/*.css",
-  ],
+  purge: {
+    enabled: process.env.NODE_ENV === "production",
+    content: [
+      "./templates/**/*.ts",
+      "./templates/**/*.tsx",
+      "./templates/**/*.js",
+      "./templates/**/*.jsx",
+      "./components/**/*.ts",
+      "./components/**/*.tsx",
+      "./components/**/*.js",
+      "./components/**/*.jsx",
+      "./styles/**/*.css",
+      "./styles/**/*.scss",
+      "./styles/**/*.sass",
+      "./styles/**/*.less",
+    ],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
