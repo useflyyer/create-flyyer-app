4a38b0477fc0c96828cfd124f94edf8695e4274a
Patricio López Juri <lopezjuripatricio@gmail.com>
2021-02-21 08:35:43 -0300

    feat: Add Flayyer breakpoints to tailwind

--- templates/react-typescript-tailwind/tailwind.config.js
+++ templates/react-typescript-tailwind/tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: {
     enabled: process.env.NODE_ENV === "production",
@@ -18,7 +20,18 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      // For apps where images are always small squares such as WhatsApp
+      "thumb": "600px",
+      // The size of an Instagram post
+      "sq": { raw: "(min-width: 1080px) and (max-height: 1080px)" },
+      // The size of an Instagram story
+      "story": { raw: "(min-width: 1080px) and (min-height: 1920px)" },
+      // The default and recommended horizontal image for sites like Facebook, Twitter, etc.
+      "banner": "1200px",
+      // Keep default Tailwind sizes
+      ...defaultTheme.screens,
+    },
   },
   variants: {
     extend: {},
