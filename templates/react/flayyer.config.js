f1b8a7d34e5ba189719c56d19524db7934e9e77e
Patricio López Juri <lopezjuripatricio@gmail.com>
2021-06-23 18:15:09 -0400

    feat: Update deps, format files and fix xo format errors

--- templates/react/flayyer.config.js
+++ templates/react/flayyer.config.js
@@ -11,6 +11,6 @@ module.exports = config({
   // Optionals
   name: '{{ replace-title }}',
   description: 'Created with create-flayyer-app',
-  private: true, // set to false to deploy publicly to https://flayyer.com/community
-  sizes: ['THUMBNAIL', 'BANNER', 'SQUARE', 'STORY'] // declare supported sizes
+  private: true, // Set to false to deploy publicly to https://flayyer.com/community
+  sizes: ['THUMBNAIL', 'BANNER', 'SQUARE', 'STORY'] // Declare supported sizes
 });
